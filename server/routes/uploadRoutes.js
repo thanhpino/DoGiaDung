a7f09f04befdc60c80e6977b7d2e35b4cc213f20
thanhpino <tt3145539@gmail.com>
2026-01-21 19:57:43 +0700

    Fix some errors

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -3,14 +3,20 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const db = require('../config/database');
+const fs = require('fs');
 
 // Cấu hình nơi lưu ảnh
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Lưu vào thư mục uploads
+        const dir = 'uploads/';
+        
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
+        cb(null, dir);
     },
     filename: (req, file, cb) => {
-        // Đặt tên file: avatar-ID-Timestamp.jpg
         cb(null, `avatar-${Date.now()}${path.extname(file.originalname)}`);
     }
 });
@@ -20,14 +26,18 @@ const upload = multer({ storage });
 // API Upload Avatar
 router.post('/upload-avatar/:userId', upload.single('avatar'), (req, res) => {
     const userId = req.params.userId;
-    const filename = req.file ? `/uploads/${req.file.filename}` : null;
+    
+    if (!req.file) return res.status(400).json("Chưa chọn file");
 
-    if (!filename) return res.status(400).json("Chưa chọn file");
+    const filename = `/uploads/${req.file.filename}`;
 
     // Lưu đường dẫn vào DB
     const sql = "UPDATE users SET avatar = ? WHERE id = ?";
     db.query(sql, [filename, userId], (err, result) => {
-        if (err) return res.status(500).json(err);
+        if (err) {
+            console.error("Lỗi update avatar vào DB:", err);
+            return res.status(500).json({ error: "Lỗi Database", details: err });
+        }
         return res.json({ status: "Success", url: filename });
     });
 });
