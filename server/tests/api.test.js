a20ec4f932e8ce7ca2677e59582992ced9062fdb
thanhpino <tt3145539@gmail.com>
2026-03-04 19:35:03 +0700

    Final deploy

--- server/tests/api.test.js
+++ server/tests/api.test.js
@@ -6,7 +6,7 @@ jest.setTimeout(15000);
 
 beforeAll(() => {
     jest.spyOn(console, 'log').mockImplementation(() => { });
-    jest.spyOn(console, 'error').mockImplementation(() => { });
+    // jest.spyOn(console, 'error').mockImplementation(() => { });
 });
 
 // ============================================
